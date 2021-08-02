e40885967c1bc7c24a498caa43f06e8f89673111
Ehdieh Khaledian <khaledianehdieh@gmail.com>
2021-08-01 18:07:32 -0700

    first commit

---
+++ tests/Test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 16:00:35 2021
+
+@author: ekhaledian
+"""
+
+from translate6frames import Translate
+
+p1 = Translate("test.fa", "DNA")
+p1.Output()
